2df873d3cb9f7a250201925485fb2377c45e206b
allnichols <allenmaxnichols@gmail.com>
2019-03-31 13:28:31 -0500

    navigation added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
 import MainNews from './MainNews/MainNews';
+import Toolbar from './navigation/Toolbar';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Toolbar />
         <MainNews />
       </div>
     );
--- src/navigation/Toolbar.js
+++ src/navigation/Toolbar.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import styles from './Toolbar.module.css';
 
 const toolbar = (props) => (
-  <header>
-      <div>Menu</div>
-      <div>Logo</div>
+  <header className={styles.Toolbar}>
+      <div>
+        <h2>The News</h2>
+      </div>
+
       <nav>
-          ...
+        ...
       </nav>
   </header>
 );
