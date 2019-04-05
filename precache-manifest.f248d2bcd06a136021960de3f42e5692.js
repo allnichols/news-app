cdd7bd52fe33b7ccb7a185f96b7459967382f14e
allnichols <allenmaxnichols@gmail.com>
2019-04-05 11:18:22 -0500

    Updates

---
+++ precache-manifest.f248d2bcd06a136021960de3f42e5692.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "fa4b01bc436166f464fa",
+    "url": "/news-app/static/js/runtime~main.dc36b5e3.js"
+  },
+  {
+    "revision": "4d148d2499e9c2250474",
+    "url": "/news-app/static/js/main.bdcb6d5f.chunk.js"
+  },
+  {
+    "revision": "d84d4737793bf17fca72",
+    "url": "/news-app/static/js/2.f5e9f5b5.chunk.js"
+  },
+  {
+    "revision": "4d148d2499e9c2250474",
+    "url": "/news-app/static/css/main.12fc91c3.chunk.css"
+  },
+  {
+    "revision": "f7d3ac540fe096a424b2366c5be9a5c4",
+    "url": "/news-app/index.html"
+  }
+];
